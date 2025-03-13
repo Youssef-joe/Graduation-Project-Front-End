5c264733fc74e393b77118ad4088ecde730a37c9
Youssef-joe <joe92228@gmail.com>
2025-03-13 15:45:13 +0200

    some changes

--- app/context/AuthContext.js
+++ app/context/AuthContext.js
@@ -1,5 +1,5 @@
 import { createContext, useContext, useEffect, useState } from "react";
-import { fetchUser } from "../utils/auth"; // Ensure this is correctly implemented
+import { fetchUser } from "../utils/auth"; 
 
 const AuthContext = createContext();
 
